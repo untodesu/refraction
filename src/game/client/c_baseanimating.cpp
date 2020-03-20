0c258f5ea00615bbda25b0253c8f403df3315318
undbsd <undunix@yandex.ru>
2020-03-20 15:37:57 +0500

    Added muzzleflash dlight

--- src/game/client/c_baseanimating.cpp
+++ src/game/client/c_baseanimating.cpp
@@ -3309,20 +3309,22 @@ void C_BaseAnimating::ProcessMuzzleFlashEvent()
 		//FIXME: We should really use a named attachment for this
 		if ( m_Attachments.Count() > 0 )
 		{
-			Vector vAttachment;
+			Vector vAttachment, vAng;
 			QAngle dummyAngles;
 			GetAttachment( 1, vAttachment, dummyAngles );
 
-			// Make an elight
-			dlight_t *el = effects->CL_AllocElight( LIGHT_INDEX_MUZZLEFLASH + index );
-			el->origin = vAttachment;
-			el->radius = random->RandomInt( 32, 64 ); 
-			el->decay = el->radius / 0.05f;
-			el->die = gpGlobals->curtime + 0.05f;
-			el->color.r = 255;
-			el->color.g = 192;
-			el->color.b = 64;
-			el->color.exponent = 5;
+			// pew-pew
+			AngleVectors( dummyAngles, &vAng );
+			vAttachment += vAng * 2;
+
+			dlight_t *dl = effects->CL_AllocDlight( index );
+			dl->origin = vAttachment;
+			dl->color.r = 127;
+			dl->color.g = 96;
+			dl->color.b = 32;
+			dl->die = gpGlobals->curtime + 0.05f;
+			dl->radius = random->RandomFloat( 32.0f, 64.0f );
+			dl->decay = 512.0f;
 		}
 	}
 }
