e56f23c5871b7153bef0a62158a302557d503250
undnull <undnull@yandex.ru>
2020-03-20 15:00:35 +0500

    Restored the Combine Soldier's ability to emit pain sounds

--- src/game/server/hl2/npc_combine.cpp
+++ src/game/server/hl2/npc_combine.cpp
@@ -2565,7 +2565,7 @@ void CNPC_Combine::SpeakSentence( int sentenceType )
 //=========================================================
 // PainSound
 //=========================================================
-void CNPC_Combine::PainSound ( void )
+void CNPC_Combine::PainSound( const CTakeDamageInfo &info )
 {
 	// NOTE: The response system deals with this at the moment
 	if ( GetFlags() & FL_DISSOLVING )
