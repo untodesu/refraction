469c2ec79fd4e0baa820d85faf7d9911d1388b41
undnull <undnull@yandex.ru>
2020-03-20 19:49:27 +0500

    Shader Cleanup. Yes, I finally installed perl and String::CRC32 module.

--- src/materialsystem/stdshaders/buildrefractionshaders.bat
+++ src/materialsystem/stdshaders/buildrefractionshaders.bat
@@ -5,10 +5,10 @@ rem ================================
 rem ==== MOD PATH CONFIGURATIONS ===
 
 rem == Set the absolute path to your mod's game directory here ==
-set GAMEDIR=%cd%\..\..\..\game\mod_episodic
+set GAMEDIR=%cd%\..\..\..\game\refraction\
 
 rem == Set the relative or absolute path to Source SDK Base 2013 Singleplayer\bin ==
-set SDKBINDIR=D:\SteamBeta\SteamApps\common\Source SDK Base 2013 Singleplayer\bin
+set SDKBINDIR=C:\Steam\steamapps\common\Source SDK Base 2013 Singleplayer\bin\
 
 rem ==  Set the Path to your mod's root source code ==
 rem This should already be correct, accepts relative paths only!
